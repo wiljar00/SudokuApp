667688c5ccc80a0812a3a8425d60c3f36aabf505
Jarom Wilcox <wiljar00@users.noreply.github.com>
2023-11-03 22:33:24 -0600

    Initial Commit

---
+++ src/com/mycompany/sudoku/Main.java
@@ -0,0 +1,15 @@
+package com.mycompany.sudoku;
+
+public class Main {
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+
+    public static void sayHello() {
+        System.out.println("Welcome to my app!");
+        System.out.println("- - - - - - - - - - ");
+        System.out.println("- - - SUDOKU! - - - ");
+        System.out.println("- - - - - - - - - - ");
+    }
+}
