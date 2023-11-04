0521478dee624c2793f3f0b9fc3b024756710b8f
Jarom Wilcox <wiljar00@users.noreply.github.com>
2023-11-03 22:52:11 -0600

    Create Sample Board
    Created example board to and print out to help visualize.

--- src/com/mycompany/sudoku/Main.java
+++ src/com/mycompany/sudoku/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         sayHello();
+
+        int[][] board = createExampleBoard();
+        printSudokuboard(board);
     }
 
     public static void sayHello() {
@@ -11,5 +14,29 @@ public class Main {
         System.out.println("- - - - - - - - - - ");
         System.out.println("- - - SUDOKU! - - - ");
         System.out.println("- - - - - - - - - - ");
+    }
+
+    public static int[][] createExampleBoard() {
+        int[][] board = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+        return board;
+    }
+
+    public static void printSudokuboard(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
